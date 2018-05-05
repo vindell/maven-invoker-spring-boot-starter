a3325f01ff5ec50adcd5183881bd7e9cd7ac6b64
jeebiz <hnxyhcwdl1003@163.com>
2018-05-05 23:02:54 +0800

    增加说明

--- src/test/java/org/apache/maven/spring/boot/Application.java
+++ src/test/java/org/apache/maven/spring/boot/Application.java
@@ -1,25 +1,19 @@
-# spring-boot-starter-maven-invoker
-Spring Boot Starter For Maven Invoker
-
-### 说明
-
- > 基于 maven-invoker 的 Spring Boot Starter 实现
-
-1. 整合 maven-invoker
-
-### Maven
-
-``` xml
-<dependency>
-	<groupId>${project.groupId}</groupId>
-	<artifactId>spring-boot-starter-maven-invoker</artifactId>
-	<version>${project.version}</version>
-</dependency>
-```
-
-### Sample
-
-```java
+/*
+ * Copyright (c) 2018, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.maven.spring.boot;
 
 import javax.annotation.PostConstruct;
 
@@ -51,5 +45,3 @@ public class Application {
 	}
 
 }
-
-```
